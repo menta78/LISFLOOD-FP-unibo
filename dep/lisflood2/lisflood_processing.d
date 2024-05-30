2a792c7067ea28ac17c3c4eefa4aa301cb00d24e
Italo Dos Reis Lopes <id22022@login1.juno.cmcc.scc>
2024-05-30 14:48:49 +0200

    new_branch

--- dep/lisflood2/lisflood_processing.d
+++ dep/lisflood2/lisflood_processing.d
@@ -2,7 +2,7 @@ build/lisflood2/lisflood_processing.o: lisflood2/lisflood_processing.cpp \
  lisflood2/../lisflood.h lisflood2/DataTypes.h lisflood2/../utility.h \
  lisflood2/lisflood2.h lisflood2/sgm_fast.h lisflood2/../rain/rain.h \
  lisflood2/../rain/../geometry.h lisflood2/file_tool.h lisflood2/../sgc.h \
- /users_home/opa/id22022/usr/numactl-2.0.15_gcc/include/numa.h
+ /users_home/cmcc/id22022/usr/numactl-2.0.15_gcc/include/numa.h
 lisflood2/../lisflood.h:
 lisflood2/DataTypes.h:
 lisflood2/../utility.h:
@@ -12,4 +12,4 @@ lisflood2/../rain/rain.h:
 lisflood2/../rain/../geometry.h:
 lisflood2/file_tool.h:
 lisflood2/../sgc.h:
-/users_home/opa/id22022/usr/numactl-2.0.15_gcc/include/numa.h:
+/users_home/cmcc/id22022/usr/numactl-2.0.15_gcc/include/numa.h:
