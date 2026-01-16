931565b4dfe816a6239e8b6c44022d58bf50fc01
ItaloRLopes <italoreislopes@gmail.com>
2026-01-16 11:07:50 +0100

    Clean files

--- compileUbuntuGnu_debug.sh
+++
@@ -1,3 +0,0 @@
-module load gcc-12.2.0/12.2.0
-export NUMADIR=/users_home/opa/id22022/usr/numactl-2.0.15_gcc
-CONFIG=config/sharc-gcc-cpu-debug make
--- compileUbuntuGnu_juno_debug.sh
+++
@@ -1,5 +0,0 @@
-module load gcc-12.2.0/12.2.0
-export NUMADIR=/users_home/cmcc/id22022/usr/numactl-2.0.15_gcc
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NUMADIR/lib
-CONFIG=config/sharc-gcc-cpu-juno-debug  make
-mv lisflood lisflood_debug
